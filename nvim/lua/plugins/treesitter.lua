dfe8965d2166adbe867e279494186f803812e7d3
Jason Su <164670+kittyflat@users.noreply.github.com>
2026-05-02 16:57:31 -0700

    Fix treesitter config using lazy main field to avoid load timing issue

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,18 +2,17 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- Replaces: vim-javascript, vim-jsx-pretty, typescript-vim, vim-tsx,
-        --           vim-json, plasticboy/vim-markdown
-        ensure_installed = {
-          "javascript", "typescript", "tsx", "json", "markdown", "markdown_inline",
-          "ruby", "python", "terraform", "lua", "vim", "vimdoc", "bash", "yaml",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
+    main = "nvim-treesitter.configs",  -- tells lazy which module to call setup on
+    -- Replaces: vim-javascript, vim-jsx-pretty, typescript-vim, vim-tsx,
+    --           vim-json, plasticboy/vim-markdown
+    opts = {
+      ensure_installed = {
+        "javascript", "typescript", "tsx", "json", "markdown", "markdown_inline",
+        "ruby", "python", "terraform", "lua", "vim", "vimdoc", "bash", "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 }
