4fc898b565b298f7ab07215460aa1d9a431c9910
Jason Su <164670+kittyflat@users.noreply.github.com>
2026-05-04 15:58:36 -0700

    Fix nvim-treesitter: use opts instead of require configs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,19 +2,16 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = "BufReadPost",
     -- Replaces: vim-javascript, vim-jsx-pretty, typescript-vim, vim-tsx,
     --           vim-json, plasticboy/vim-markdown
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "javascript", "typescript", "tsx", "json", "markdown", "markdown_inline",
-          "ruby", "python", "terraform", "lua", "vim", "vimdoc", "bash", "yaml",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        "javascript", "typescript", "tsx", "json", "markdown", "markdown_inline",
+        "ruby", "python", "terraform", "lua", "vim", "vimdoc", "bash", "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 }
