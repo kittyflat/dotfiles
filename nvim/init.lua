4c7af84004c70d88f0a35cb0dbfd36be32fa01b9
Jason Su <164670+kittyflat@users.noreply.github.com>
2026-05-05 01:48:58 -0700

    nvim: fix vim.loop deprecation and add ghostty syntax support
    - Replace vim.loop with vim.uv (deprecated in neovim 0.10+)
    - Add Ghostty app bundle to lazy rtp so ghostty filetype gets syntax highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
@@ -19,4 +19,9 @@ require("autocmds")
 
 require("lazy").setup("plugins", {
   change_detection = { notify = false },
+  performance = {
+    rtp = {
+      paths = { "/Applications/Ghostty.app/Contents/Resources/nvim/site" },
+    },
+  },
 })
