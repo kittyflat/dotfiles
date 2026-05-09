530347833c15563d768a7167f3a3b7d9c9ad7766
Jas Sy <164670+kittyflat@users.noreply.github.com>
2026-05-09 01:29:38 -0700

    nvim: telescope improvements
    - Show hidden and gitignored files in find_files via fd
    - Sort results ascending (top to bottom) in bottom pane layout
    - Disable live_grep preview by default (toggle with Alt-P)
    - Add \ff, \fg, \fw, \fb, \fr, \fR, \fh, \fk keymaps
    - Add fd to Brewfile

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,19 +6,40 @@ return {
       -- Native fzf sorter for faster fuzzy matching
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
+    keys = {
+      { "<C-p>",      "<CMD>Telescope find_files<CR>",                                                               desc = "Find files" },
+      { "<C-f>",      "<CMD>Telescope grep_string<CR>",                                                              desc = "Find word under cursor" },
+      { "<leader>ff", "<CMD>Telescope find_files<CR>",                                                               desc = "Find files" },
+      { "<leader>fg", "<CMD>Telescope live_grep<CR>",                                                                desc = "Live grep" },
+      { "<leader>fw", "<CMD>Telescope grep_string<CR>",                                                              desc = "Find word under cursor" },
+      { "<leader>fb", "<CMD>Telescope buffers<CR>",                                                                  desc = "Buffers" },
+      { "<leader>fr", function() require("telescope.builtin").oldfiles({ cwd_only = true }) end,                     desc = "Recent files (project)" },
+      { "<leader>fR", "<CMD>Telescope oldfiles<CR>",                                                                 desc = "Recent files (all)" },
+      { "<leader>fh", "<CMD>Telescope help_tags<CR>",                                                                desc = "Help tags" },
+      { "<leader>fk", "<CMD>Telescope keymaps<CR>",                                                                  desc = "Keymaps" },
+    },
     config = function()
       local telescope = require("telescope")
       telescope.setup({
         defaults = {
           layout_strategy = "bottom_pane",
           layout_config = {
-            height = 0.5,         -- taller pane, more room for results
-            preview_width = 0.45, -- less preview, more results width
+            height = 0.5,
+            preview_width = 0.45,
           },
+          sorting_strategy = "ascending",
           mappings = {
             i = {
               ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
             },
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "fd", "--type", "f", "--hidden", "--no-ignore-vcs", "--exclude", ".git", "--exclude", "node_modules" },
+          },
+          live_grep = {
+            previewer = false,
           },
         },
       })
